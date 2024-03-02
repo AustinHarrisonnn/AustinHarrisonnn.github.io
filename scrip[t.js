023dc7aad49926c43c2e71df33972ea80ac3cdb5
Austin Harrison <147441363+AustinHarrisonnn@users.noreply.github.com>
2024-03-02 12:18:36 -0500

    Add files via upload

---
+++ scrip[t.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var row = document.querySelector(".row");
+    // Your JavaScript logic for the row goes here
+    // For example: row.style.marginTop = "20vh";
+});
